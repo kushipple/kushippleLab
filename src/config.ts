01e7ea3968836ecee39d68455c1399b6c61e27c4
kushipple <heeee23ys.kushi@gmail.com>
2024-10-20 12:00:57 +0900

    steam_and_X

--- src/config.ts
+++ src/config.ts
@@ -56,12 +56,12 @@ export const profileConfig: ProfileConfig = {
       icon: 'fa6-brands:twitter',       // Visit https://icones.js.org/ for icon codes
                                         // You will need to install the corresponding icon set if it's not already included
                                         // `pnpm add @iconify-json/<icon-set-name>`
-      url: 'https://twitter.com',
+      url: 'https://x.com/kushipple',
     },
     {
       name: 'Steam',
       icon: 'fa6-brands:steam',
-      url: 'https://store.steampowered.com',
+      url: 'https://steamcommunity.com/id/kushipple/',
     },
     {
       name: 'GitHub',
